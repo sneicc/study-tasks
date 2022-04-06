e24761fbc0f8f68376782eb782a5746762cf6549
Andrey <102552777+sneicc@users.noreply.github.com>
2022-04-06 21:50:47 +0300

    add lexer class

---
+++ Java/my_language/lang_1.8/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String content = Files.lines(Paths.get("Code.txt")).reduce("",String::concat);
+        System.out.println(content);
+        Lexer lexer = new Lexer();
+        lexer.defType(content).stream()
+                .forEach(l -> System.out.println(l.content+" "+l.type));
+
+    }
+
+}
