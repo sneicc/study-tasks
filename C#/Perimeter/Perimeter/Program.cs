5ff867e658916ff32aa645419551f2c714835b3d
Andrey <102552777+sneicc@users.noreply.github.com>
2022-05-08 18:23:52 +0300

    1

---
+++ C#/Perimeter/Perimeter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Perimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FibPerimeter(1000000) * 4);
+        }
+        public static BigInteger FibPerimeter(int n)
+        {
+            BigInteger before1 = 0;
+            BigInteger before2 = 1;
+            BigInteger now;
+            BigInteger ans = 0;
+            for (int i = 0; i < n + 1; i++)
+            {
+                now = before1 + before2;
+                before2 = before1;
+                before1 = now;
+                ans += now;
+            }
+            return ans;
+        }
+    }
+}
