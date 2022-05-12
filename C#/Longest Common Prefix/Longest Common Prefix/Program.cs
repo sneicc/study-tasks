321293307a33256c2a1d8fbe477b82f223c21c75
Andrey <102552777+sneicc@users.noreply.github.com>
2022-05-12 22:57:10 +0300

    1

--- C#/Longest Common Prefix/Longest Common Prefix/Program.cs
+++ C#/Longest Common Prefix/Longest Common Prefix/Program.cs
@@ -10,6 +10,26 @@ namespace Longest_Common_Prefix
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(LongestCommonPrefix(new string[] { "flower", "flow", "flight" }));
+        }
+
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            //var sortStrs = strs.OrderBy(x => x).ToArray();
+            StringBuilder sb = new StringBuilder();
+            Array.Sort(strs, (x, y) => x.Length.CompareTo(y.Length));  
+            string firstString = strs[0];
+
+            for (int i = 0; i < firstString.Length; i++)
+            {
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    if (firstString[i] != strs[j][i])
+                        return sb.ToString();
+                }
+                sb.Append(firstString[i]);
+            }                
+            return sb.ToString();
         }
     }
 }
