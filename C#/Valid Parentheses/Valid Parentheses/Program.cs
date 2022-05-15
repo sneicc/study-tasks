78b6de6145e8734fd069f962a79ad00716ff0e3b
Andrey <102552777+sneicc@users.noreply.github.com>
2022-05-15 18:33:42 +0300

    1

---
+++ C#/Valid Parentheses/Valid Parentheses/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Parentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("[(()[]{})]"));
+        }
+
+        public static bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var item in s)
+            {
+                if (item == ')' || item == ']' || item == '}')
+                {
+                    if (stack.Count == 0)
+                        return false;
+                }
+                
+                switch (item)
+                {
+                    case ')':
+                        if ('(' != stack.Pop())
+                            return false;
+                        break;
+                    case '}':
+                        if ('{' != stack.Pop())
+                            return false;
+                        break;
+                    case ']':
+                        if ('[' != stack.Pop())
+                            return false;
+                        break;
+                    default:
+                        stack.Push(item);
+                        break;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
